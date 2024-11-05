2e3d71a9ee6da6d042f5b9ff89552f64fc9fe25b
SAKMADD <ahmadsafii.work@gmail.com>
2024-11-05 15:14:15 +0700

    2

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,7 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 
@@ -27,7 +22,7 @@ async function main() {
   });
 
   app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at port : ${port}`);
   });
 }
 
